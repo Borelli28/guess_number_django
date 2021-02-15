fd9d7221f0cc7b949db087d994b03c80106b8e56
armando <borelliarmando12@gmail.com>
2021-02-14 18:38:26 -0600

    Added clear method and update guess method. Both are working and compare if statement are working too. Now working on making sure the random number is not being update until the user guess the number.

--- number_app/urls.py
+++ number_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('clear', views.clear),
+    path('update_guess', views.update_guess)
 ]
--- number_app/views.py
+++ number_app/views.py
@@ -1,31 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import random
 
+# python manage.py
 
 def index(request):
-    #Create a rand numer between 1 - 100
-    the_number = random.randint(1, 100)
-    print("The cpu generated number is: " + str(the_number))
 
-    # Save the random number in sessions
-    request.session['number'] = the_number
-    print(request.session['key'])
+    #create random number
+    request.session['number'] = random.randint(1, 100)
 
-    # If a guess number already exist then
+    # if user guess is not empty
     if 'guess' in request.session:
-        print("Guess exists")
-        print(request.session['guess'])
-        # if there a new guess inputted by user then update 'guess'
-        if request.session['guess'] != request.POST['user_guess']:
-            print("There is a new user guess so 'guess' is being updated in session")
-            request.session['guess'] = request.POST['user_guess']
-            print(request.session['guess'])
 
-
-    # Else if guess does not exist create one
-    else:
-        request.session['guess'] = request.POST['user_guess']
+        print("Current user guess:")
         print(request.session['guess'])
+
+        # User guess is greater than the random number
+        if request.session['number'] < int(request.session['guess']):
+            print("User Guess is greater than the number")
+
+        # User guess is less than the random number
+        elif request.session['number'] > int(request.session['guess']):
+            print("User Guess is less than the number")
+
+        # if user guess the number then generate a new random number
+        elif request.session['number'] == int(request.session['guess']):
+            # print(request.session['number'] + request.session['guess'])
+            print("User guess the number!!!!!")
+            request.sessioon['number'] = random.randint(1, 100)
+            print("New random number: ")
+            print(request.session['number'])
+
+    print("The random number is: ")
+    print(request.session['number'])
 
     #Render page
     return render(request, 'index.html')
+
+
+# Udpates the user guess number then redirect back to root
+def update_guess(request):
+    request.session['guess'] = request.POST['user_guess']
+    #prints the current user guess
+    print("User guess updated")
+    print(request.session['guess'])
+
+    return redirect('/')
+
+
+
+# Clear sessions and redirect to root
+def clear(request):
+    request.session.clear()
+    print("Session was cleared")
+    return redirect('/')
+
+
