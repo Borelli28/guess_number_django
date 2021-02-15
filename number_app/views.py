0875788f5a60a071fcd7249a14492095eac44b03
armando <borelliarmando12@gmail.com>
2021-02-14 20:49:59 -0600

    Program runs fine, except if the user enter a anything that is not a number it will break the program, I could not figure out how to fix that.

--- number_app/views.py
+++ number_app/views.py
@@ -6,10 +6,14 @@ import random
 
 def index(request):
 
+    # If user input is a word it will break the program and will need to reset the guess number manually by uncommenting the line below
     # request.session['guess'] = 666
 
     # if user guess is not empty
     if 'guess' in request.session and 'number' in request.session:
+
+        # create a display variable in session
+        request.session['display'] = ""
 
         print("Current user guess:")
         print(request.session['guess'])
@@ -17,15 +21,18 @@ def index(request):
         # User guess is greater than the random number
         if request.session['number'] < int(request.session['guess']):
             print("User Guess is greater than the number")
+            request.session['display'] = "Too High!"
 
         # User guess is less than the random number
         elif request.session['number'] > int(request.session['guess']):
             print("User Guess is less than the number")
+            request.session['display'] = "Too Low!"
 
         # if user guess the number then generate a new random number
         elif request.session['number'] == int(request.session['guess']):
             # print(request.session['number'] + request.session['guess'])
             print("User guess the number!!!!!")
+            request.session['display'] = f"You got it! {request.session['number']} was the number... CONGRATS!"
             request.session['number'] = random.randint(1, 100)
             print("New random number: ")
             print(request.session['number'])
@@ -36,12 +43,8 @@ def index(request):
 
 # Udpates the user guess number then redirect back to root
 def update_guess(request):
-    print("Inside update_guess")
     # If user input not empty and greater than 0 and also is a integer(check that is not a word)
     if request.POST['user_guess'] != "" and int(request.POST['user_guess']) > 0:
-        # lol = request.POST['user_guess']
-        # print(lol.isnumeric())
-        print("The input went trough the if statement")
         request.session['guess'] = request.POST['user_guess']
         # prints the current user guess
         print("User guess updated")
